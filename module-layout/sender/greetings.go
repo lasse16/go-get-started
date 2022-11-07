df1e664d7562b18f7cb33797365730a4915a1d32
Lasse Haffke <lasse@haffke.me>
2022-11-07 15:47:32 +0100

    Add random greeting generation

--- module-layout/sender/greetings.go
+++ module-layout/sender/greetings.go
@@ -3,12 +3,28 @@ package sender
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("No name given")
 	}
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomGreeting(), name)
 	return message, nil
+}
+
+func randomGreeting() string {
+	greetings := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+	return greetings[rand.Intn(len(greetings))]
+
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
 }
