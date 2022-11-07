35be8a323c3bfb2a59fdb7b32fe05a353e4d6528
Lasse Haffke <lasse@haffke.me>
2022-11-07 15:26:06 +0100

    Add error handling code in caller

--- module-layout/caller/greeter.go
+++ module-layout/caller/greeter.go
@@ -3,9 +3,15 @@ package main
 import (
 	"fmt"
 	"get-started/sender"
+	"log"
 )
 
 func main() {
-	message := sender.Hello("TEST")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+	message, err := sender.Hello("TEST")
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(message)
 }
